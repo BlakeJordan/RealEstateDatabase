8c34504d3eabef05b9480017a36981dc713e7dda
BlakeJordan <Blake.Jordan@wellsky.com>
2020-05-15 16:44:56 -0500

    login fix

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT userid FROM Users WHERE userid = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
@@ -19,8 +19,6 @@
 		
       if($count == 1) {
          session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
          header("location: home.html");
       }else {
          $error = "Your Login Name or Password is invalid";
