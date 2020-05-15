cfaf4b67f655a43622a757126cb6592690972bd2
kevdinh33 <kevdinh33@gmail.com>
2020-05-15 17:58:12 -0500

    Fixed user adding

--- admin/php/AddUser.php
+++ admin/php/AddUser.php
@@ -1,7 +1,7 @@
 <?php
 echo "<link rel='stylesheet' href='style.css'>";
-$userid =$_POST['sellerAddress'];
-$password = $_POST['sellerEmail'];
+$userid =$_POST['Username'];
+$password = $_POST['Password'];
 $mysqli = new mysqli("mysql.eecs.ku.edu", "kevdinh33", "Meej3ien", "kevdinh33");
 
 if ($mysqli->connect_error){
@@ -16,7 +16,7 @@ if($user != "NULL"){
   
   //Success output
   if ($mysqli->query($query) === TRUE) {
-    echo "<h1>Seller has been successfully added.</h1>";
+    echo "<h1>User has been successfully added.</h1>";
   }
   else if($user == "NULL"){
     echo "<h1>No fields can be left blank.<h1>";
@@ -30,6 +30,6 @@ if($user != "NULL"){
     <button type='button'>Return to Admin Home</button>
     </a>
   </div>";
-  
+
   $mysqli->close();
    ?>
