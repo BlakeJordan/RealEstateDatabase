6cbf4ecda069316ffed73db656d5cbc45eca4792
kevdinh33 <kevdinh33@gmail.com>
2020-05-15 18:55:45 -0500

    user add

--- login.php
+++ login.php
@@ -3,16 +3,16 @@
    session_start();
    echo "<h1>Buyer has been successfully added0.</h1>";
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
+      // username and password sent from form
+
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
       $sql = "SELECT userid FROM Users WHERE userid = '$myusername' and password= '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-      
+
       $count = mysqli_num_rows($result);
 		echo "<h1>Buyer has been successfully added.</h1>";
       if($count == 1) {
