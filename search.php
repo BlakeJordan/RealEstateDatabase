db161a146dc8ca42b0583b2f676e5607415ae384
Eric <erjseals@gmail.com>
2020-05-15 18:41:56 -0500

    first query

--- search.php
+++ search.php
@@ -1,153 +1,179 @@
 <?php
-   $mysqli = new mysqli("mysql.eecs.ku.edu", "kevdinh33", "Meej3ien", "kevdinh33");
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "kevdinh33", "Meej3ien", "kevdinh33");
 
-   $option = isset($_POST['tableOptions']) ? $_POST['tableOptions'] : false;
+    $search1 = isset($_POST['house-price']) ? $_POST['house-price'] : false;
+    $option = isset($_POST['tableOptions']) ? $_POST['tableOptions'] : false;
 
-   if ($mysqli->connect_errno)
-   {
-     printf("Connect failed: %s\n", $mysqli->connect_error);
-     exit();
-   }
+    if ($mysqli->connect_errno)
+    {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
 
-   if($option[0] == '1'){
-        $result = mysqli_query($mysqli,"SELECT * FROM Buyers");
+    if($search1){
+        $result = mysqli_query($mysqli,"SELECT DISTINCT Sellers.LastName, Sellers.FirstName
+                                        FROM Sellers
+                                        INNER JOIN Houses ON (Sellers.SellerID = Houses.OwnerID)
+                                        WHERE Sellers.SellerID IN ( SELECT OwnerID FROM Houses
+                                        GROUP BY OwnerID HAVING count(*) > 1);");
 
         echo "<table border='1'>
         <tr>
-        <th>Email</th>
         <th>LastName</th>
         <th>FirstName</th>
-        <th>Phone</th>
-        <th>BuyerID</th>
         </tr>";
-        
+
         while($row = mysqli_fetch_array($result))
         {
         echo "<tr>";
-        echo "<td>" . $row['Email'] . "</td>";
         echo "<td>" . $row['LastName'] . "</td>";
         echo "<td>" . $row['FirstName'] . "</td>";
-        echo "<td>" . $row['Phone'] . "</td>";
-        echo "<td>" . $row['BuyerID'] . "</td>";
         echo "</tr>";
         }
         echo "</table>";
     }
 
-    elseif($option[0] == '2'){
-        $result = mysqli_query($mysqli,"SELECT * FROM Sellers");
+    else {
+        if($option[0] == '1'){
+            $result = mysqli_query($mysqli,"SELECT * FROM Buyers");
 
-        echo "<table border='1'>
-        <tr>
-        <th>Address</th>
-        <th>LastName</th>
-        <th>FirstName</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>SellerID</th>
-        </tr>";
-        
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['Address'] . "</td>";
-        echo "<td>" . $row['LastName'] . "</td>";
-        echo "<td>" . $row['FirstName'] . "</td>";
-        echo "<td>" . $row['Email'] . "</td>";
-        echo "<td>" . $row['Phone'] . "</td>";
-        echo "<td>" . $row['SellerID'] . "</td>";
-        echo "</tr>";
+            echo "<table border='1'>
+            <tr>
+            <th>Email</th>
+            <th>LastName</th>
+            <th>FirstName</th>
+            <th>Phone</th>
+            <th>BuyerID</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['Email'] . "</td>";
+            echo "<td>" . $row['LastName'] . "</td>";
+            echo "<td>" . $row['FirstName'] . "</td>";
+            echo "<td>" . $row['Phone'] . "</td>";
+            echo "<td>" . $row['BuyerID'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
         }
-        echo "</table>";
-    }
-    elseif($option[0] == '6'){
-        $result = mysqli_query($mysqli,"SELECT * FROM Users");
 
-        echo "<table border='1'>
-        <tr>
-        <th>UserID</th>
-        <th>Password</th>
-        </tr>";
-        
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['userid'] . "</td>";
-        echo "<td>" . $row['password'] . "</td>";
-        echo "</tr>";
-        }
-        echo "</table>";
-    }
-    elseif($option[0] == '3'){
-        $result = mysqli_query($mysqli,"SELECT * FROM Houses");
+        elseif($option[0] == '2'){
+            $result = mysqli_query($mysqli,"SELECT * FROM Sellers");
 
-        echo "<table border='1'>
-        <tr>
-        <th>Address</th>
-        <th>Square Footage</th>
-        <th>Number of Rooms</th>
-        <th>Number of Baths</th>
-        </tr>";
-        
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['Address'] . "</td>";
-        echo "<td>" . $row['SquareFootage'] . "</td>";
-        echo "<td>" . $row['RoomNums'] . "</td>";
-        echo "<td>" . $row['BathNums'] . "</td>";
-        echo "</tr>";
+            echo "<table border='1'>
+            <tr>
+            <th>Address</th>
+            <th>LastName</th>
+            <th>FirstName</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>SellerID</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['Address'] . "</td>";
+            echo "<td>" . $row['LastName'] . "</td>";
+            echo "<td>" . $row['FirstName'] . "</td>";
+            echo "<td>" . $row['Email'] . "</td>";
+            echo "<td>" . $row['Phone'] . "</td>";
+            echo "<td>" . $row['SellerID'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
         }
-        echo "</table>";
-    }
-    elseif($option[0] == '4'){
-        $result = mysqli_query($mysqli,"SELECT * FROM EstateAgency");
+        elseif($option[0] == '6'){
+            $result = mysqli_query($mysqli,"SELECT * FROM Users");
 
-        echo "<table border='1'>
-        <tr>
-        <th>Licence</th>
-        <th>Name</th>
-        <th>Contracts</th>
-        <th>House List</th>
-        <th>Agent ID</th>
-        </tr>";
-        
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['License'] . "</td>";
-        echo "<td>" . $row['Name'] . "</td>";
-        echo "<td>" . $row['Contracts'] . "</td>";
-        echo "<td>" . $row['HouseList'] . "</td>";
-        echo "<td>" . $row['AgentID'] . "</td>";
-        echo "</tr>";
+            echo "<table border='1'>
+            <tr>
+            <th>UserID</th>
+            <th>Password</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['userid'] . "</td>";
+            echo "<td>" . $row['password'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
         }
-        echo "</table>";
-    }
-    elseif($option[0] == '5'){
-        $result = mysqli_query($mysqli,"SELECT * FROM Loans");
+        elseif($option[0] == '3'){
+            $result = mysqli_query($mysqli,"SELECT * FROM Houses");
 
-        echo "<table border='1'>
-        <tr>
-        <th>ULI</th>
-        <th>Bank Loan</th>
-        <th>Interest Rate</th>
-        <th>Mortgage Length</th>
-        </tr>";
-        
-        while($row = mysqli_fetch_array($result))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['ULI'] . "</td>";
-        echo "<td>" . $row['BankLoan'] . "</td>";
-        echo "<td>" . $row['InterestRate'] . "</td>";
-        echo "<td>" . $row['MortgageLength'] . "</td>";
-        echo "</tr>";
+            echo "<table border='1'>
+            <tr>
+            <th>Address</th>
+            <th>Square Footage</th>
+            <th>Number of Rooms</th>
+            <th>Number of Baths</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['Address'] . "</td>";
+            echo "<td>" . $row['SquareFootage'] . "</td>";
+            echo "<td>" . $row['RoomNums'] . "</td>";
+            echo "<td>" . $row['BathNums'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
         }
-        echo "</table>";
+        elseif($option[0] == '4'){
+            $result = mysqli_query($mysqli,"SELECT * FROM EstateAgency");
+
+            echo "<table border='1'>
+            <tr>
+            <th>Licence</th>
+            <th>Name</th>
+            <th>Contracts</th>
+            <th>House List</th>
+            <th>Agent ID</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['License'] . "</td>";
+            echo "<td>" . $row['Name'] . "</td>";
+            echo "<td>" . $row['Contracts'] . "</td>";
+            echo "<td>" . $row['HouseList'] . "</td>";
+            echo "<td>" . $row['AgentID'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
+        }
+        elseif($option[0] == '5'){
+            $result = mysqli_query($mysqli,"SELECT * FROM Loans");
+
+            echo "<table border='1'>
+            <tr>
+            <th>ULI</th>
+            <th>Bank Loan</th>
+            <th>Interest Rate</th>
+            <th>Mortgage Length</th>
+            </tr>";
+            
+            while($row = mysqli_fetch_array($result))
+            {
+            echo "<tr>";
+            echo "<td>" . $row['ULI'] . "</td>";
+            echo "<td>" . $row['BankLoan'] . "</td>";
+            echo "<td>" . $row['InterestRate'] . "</td>";
+            echo "<td>" . $row['MortgageLength'] . "</td>";
+            echo "</tr>";
+            }
+            echo "</table>";
+        }
+        else{
+            echo "<h1>" . $option . "</h1>";
+        }
     }
-    else{
-        echo "<h1>" . $option . "</h1>";
-    }
-   mysqli_close($mysqli);
+    mysqli_close($mysqli);
 ?>
