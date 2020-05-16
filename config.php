e98967c2ad0faa074411b66ad2b1b7c9dbdeed76
kevdinh33 <kevdinh33@gmail.com>
2020-05-15 19:03:56 -0500

    merge

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'mysql.eecs.ku.edu');
-   define('DB_USERNAME', 'kevdinh33');
-   define('DB_PASSWORD', 'Meej3ien');
-   define('DB_DATABASE', 'kevdinh33');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
